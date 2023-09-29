be9a89a6a55faedf2ee20ef53c0ff38944071ec4
Dami-Karv <114776598+Dami-Karv@users.noreply.github.com>
2023-09-29 15:24:22 +0200

    Update grid-splitter.py
    now works for all splits (2 4 6 8 9 16)

--- grid-splitter.py
+++ grid-splitter.py
@@ -3,14 +3,23 @@ from PyPDF2 import PageObject, Transformation
 from copy import copy
 
 def split_pdf(input_file, output_file):
-    # Create a PDF reader and writer object
+    valid_splits = [2, 4, 6, 8, 9, 16]
+    num_splits = int(input(f"Enter the number of splits per page {valid_splits}: "))
+    
+    while num_splits not in valid_splits:
+        print(f"Invalid input! Please enter one of {valid_splits}")
+        num_splits = int(input(f"Enter the number of splits per page {valid_splits}: "))
+    
+    if num_splits == 2 or num_splits == 6:
+        num_splits_horizontal = 2
+        num_splits_vertical = num_splits // 2
+    else:
+        num_splits_horizontal = int(num_splits ** 0.5)
+        num_splits_vertical = num_splits // num_splits_horizontal
+    
     pdf_reader = PyPDF2.PdfReader(input_file)
     pdf_writer = PyPDF2.PdfWriter()
-    
-    # Assuming the 9 pages are arranged in a 3x3 grid
-    num_splits_vertical = 3
-    num_splits_horizontal = 3
-    
+
     for page_num in range(len(pdf_reader.pages)):
         page = pdf_reader.pages[page_num]
         
@@ -20,8 +29,7 @@ def split_pdf(input_file, output_file):
         width_per_split = page_width / num_splits_horizontal
         height_per_split = page_height / num_splits_vertical
         
-        # Crop and add each section of the combined page to the new PDF
-        for y in reversed(range(num_splits_vertical)):     # Use reversed to count down
+        for y in reversed(range(num_splits_vertical)):
             for x in range(num_splits_horizontal):
                 lower_left_x = x * width_per_split
                 lower_left_y = y * height_per_split
@@ -29,8 +37,6 @@ def split_pdf(input_file, output_file):
                 upper_right_y = (y + 1) * height_per_split
 
                 new_page = PageObject.create_blank_page(width=width_per_split, height=height_per_split)
-                
-                # Deep copy the original page for transformations
                 page_copy = copy(page)
                 page_copy.add_transformation(Transformation().translate(-lower_left_x, -lower_left_y))
                 new_page.merge_page(page_copy)
@@ -39,10 +45,11 @@ def split_pdf(input_file, output_file):
                 new_page.mediabox.upper_right = (width_per_split, height_per_split)
 
                 pdf_writer.add_page(new_page)
-    
-    # Write the split pages to a new PDF file
+
     with open(output_file, 'wb') as output:
         pdf_writer.write(output)
 
-# Usage
-split_pdf('theoria.pdf', 'split.pdf')
+input_file = input("Enter the path of the input PDF file: ")
+output_file = input("Enter the path of the output PDF file: ")
+
+split_pdf(input_file, output_file)
